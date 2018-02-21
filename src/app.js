9da694418289bb73c9fdd8ede8109d8c14c5ed2c
Muhammad Dadu <muhammad@dadu.co>
2018-02-21 23:23:26 +0000

    Make mobile just right

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 import './style/_module.scss';
 
+const DEBOUNCE_MS = 200;
+const SETVIEWPORTHEIGHT_SELECTOR = 'js-setviewportsize';
+
 class App {
     constructor() {
+        this._cache = {};
+        this.loadActions();
+    }
+
+    loadActions() {
+        window.addEventListener('resize', () => this.onResize());
+        this.onResize();
+    }
+
+    onResize() {
+        this._cache.viewport = null;
+        this.setViewportSize();
+    }
+
+    updateViewportInformation() {
+        return this._cache.viewport = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+        };
+    }
+
+    getViewportInformation() {
+        return this._cache.viewport || this.updateViewportInformation();
+    }
+
+    setViewportSize($el) {
+        let elements = document.querySelector(`.${SETVIEWPORTHEIGHT_SELECTOR}`);
+        let viewportSize = this.getViewportInformation();
+
+        elements.setAttribute('style', `max-width: ${viewportSize.width}px; max-height: ${viewportSize.height}px`);
     }
 }
 
